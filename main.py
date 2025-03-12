cd7b51a480e17eaff62508115c6833e72c362eb2
Joonas Symbio <joonas.malila@symbio.com>
2025-03-12 07:29:36 +0200

    Adding error handling to functions, type validation to functions

--- main.py
+++ main.py
@@ -7,28 +7,36 @@ db = client.mydb
 
 users = db.users
 
-def insert(to_insert):
+def insert_multiple(to_insert: list):
     """Inserts many new user records to the database"""
-    user_id = users.insert_many(to_insert).inserted_ids
-    print(f"User record created with id: {user_id}")
+    try:
+        user_id = users.insert_many(to_insert).inserted_ids
+        print(f"User record created with id: {user_id}")
+    except:
+        print("Could not insert record.")
 
-
-def update(to_update, update_to):
+def update(to_update, update_to: dict):
     """Updates one record from the database"""
-    users.update_one(to_update, update_to)
-    print("Record updated.")
+    try:
+        users.update_one(to_update, update_to)
+        print("Record updated.")
+    except:
+        print("Could not update record.")
 
 def delete(item_to_delete: dict):
     """Deletes one record from the database"""
-    users.delete_one(item_to_delete)
-    print("Record deleted.")
+    try:
+        users.delete_one(item_to_delete)
+        print("Record deleted.")
+    except:
+        print("Could not delete the record")
 
 user1 = {"username": "joonas", "password": "123", "favorite_nmbr": "007", "hobbies": ["bowling", "nature", "gym"]}
 user2 = {"username": "janne", "password": "hei101", "favorite_food": "pasta" , "hobbies": ["books", "cars", "skiing"]}
 
 # Add new user records
 new_users =[user1, user2]
-insert(new_users)
+insert_multiple(new_users)
 
 # Update records
 to_update = {"username": "janne"}
